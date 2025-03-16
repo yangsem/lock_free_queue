cf0bf7709f93c2ccc5ba7980f0db4a786126b3a9
yangsem <yangsemcs@gmail.com>
2025-03-16 21:33:33 +0800

    chore: googletest-1.12.x.zip

---
+++ unittest/googletest/build_googletest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+pwd=$(PWD)
+gtest_lib=${pwd}/lib
+
+rm -rf ${gtest_lib}
+rm -rf ${pwd}/googletest-1.12.x
+unzip googletest-1.12.x.zip
+cd googletest-1.12.x
+mkdir -p build
+cd build
+cmake -DCMAKE_CXX_STANDARD=11 ..
+make -j6
+
+mkdir -p ${gtest_lib}
+cp -r lib/* ${gtest_lib}
+
+cd ${pwd}
+rm -rf googletest-1.12.x
+
+echo "googletest build and library copy completed."
